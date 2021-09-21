30ae2232dc5b5fce9544ec10b7ef970f47e3ecef
jzhao20 <jzhaotxus@gmail.com>
2021-09-20 21:49:15 -0500

    passing the tests

--- models.py
+++ models.py
@@ -12,36 +12,36 @@ class DAN(nn.Module):
     def __init__(self,input, hidden,output,embedding,batch=1,drop_out_prob=.2):
         super(DAN,self).__init__()
         self.device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        #input is the size
         self.word_indexer=embedding.word_indexer
         weights=torch.FloatTensor(embedding.vectors)
         self.embedded=nn.Embedding.from_pretrained(weights,padding_idx=0).requires_grad_(False)
-        #self.drop_out_1=nn.Dropout(drop_out_prob)
         self.V=nn.Linear(input,hidden)
-        #self.drop_out_2=nn.Dropout(drop_out_prob)
         self.g=nn.Tanh()
         self.W=nn.Linear(hidden,output)
-        self.log_softmax=nn.LogSoftmax(dim=1)
+        self.log_softmax=nn.LogSoftmax(dim=0)
         nn.init.xavier_uniform_(self.V.weight)
         nn.init.xavier_uniform_(self.W.weight)
-    def forward(self,x,batch_size):
+    def forward(self,x,batch_size=1):
         #convert this list to a np list of indexes and then convert to tensor
-        max_length=0
-        for batch in x:
-            max_length=max(len(batch),max_length)
-        input=np.zeros((batch_size,max_length),dtype=np.int64)
-        for batch in range(0,batch_size):
-            for i in range(0,len(x[batch])):
-                index=self.word_indexer.index_of(x[batch][i])
-                input[batch][i]=index if index!=-1 else self.word_indexer.index_of("UNK")
+        if batch_size!=1:
+            max_length=0
+            for batch in x:
+                max_length=max(len(batch),max_length)
+            input=np.zeros((max_length,batch_size),dtype=np.int64)
+            for batch in range(0,batch_size):
+                for i in range(0,len(x[batch])):
+                    index=self.word_indexer.index_of(x[batch][i])
+                    input[i][batch]=index if index!=-1 else self.word_indexer.index_of("UNK")
+        else:
+            input=np.zeros(len(x))
+            for i in range (0,len(x)):
+                index=self.word_indexer.index_of(x[i])
+                input[i]=index if index!=-1 else self.word_indexer.index_of("UNK")
         input=self.embedded(torch.LongTensor(input).to(self.device))
-        input=input.mean(1)
-        #input=self.drop_out_1(input)
-        #input=self.drop_out_2(hidden_layer)
+        input=input.mean(0)
         input=self.V(input)
         input=self.W(self.g(input))
-        input_to_softmax=input
-        return self.log_softmax(input_to_softmax)
+        return self.log_softmax(input)
 
 class SentimentClassifier(object):
     """
@@ -86,9 +86,8 @@ class NeuralSentimentClassifier(SentimentClassifier):
         self.model=dan
     def predict(self, dev_ex:List[str]):
         dan=self.model
-        log_probs=dan.forward(dev_ex,1)
+        log_probs=dan.forward(dev_ex)
         return torch.argmax(log_probs)
-
 
 def train_deep_averaging_network(args, train_exs: List[SentimentExample], dev_exs: List[SentimentExample], word_embeddings: WordEmbeddings) -> NeuralSentimentClassifier:
     """
@@ -101,29 +100,39 @@ def train_deep_averaging_network(args, train_exs: List[SentimentExample], dev_ex
     num_epochs=10
     input_size=word_embeddings.get_embedding_length()
     num_classes=2
-    embedding_size=20
+    hidden_size=50
     batch_size=1
-    dan=DAN(input_size,embedding_size,num_classes,word_embeddings,batch_size)
+    dan=DAN(input_size,hidden_size,num_classes,word_embeddings,batch_size)
     device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     dan.to(device)
-    initial_learning_rate=.1
+    initial_learning_rate=.001
     optimizer=optim.Adam(dan.parameters(),lr=initial_learning_rate)
     for epoch in range(0,num_epochs):
-        random.seed(0)
         random.shuffle(train_exs)
         total_loss=0
         for i in range(0,len(train_exs)-batch_size+1):
-            x=[train_exs[j].words for j in range(i,i+batch_size)]
-            y=[[train_exs[j].label==0,train_exs[j].label==1] for j in range(i,i+batch_size)]
-            y_onehot=torch.from_numpy(np.asarray(y,dtype=np.int64)).to(device).float()
-            dan.zero_grad()
-            log_probs=dan.forward(x,batch_size)
-            loss_matrix=torch.matmul(torch.neg(log_probs).t(),y_onehot)
-            #equal to the sum of the main diagnol
-            loss=torch.sum(torch.diagonal(loss_matrix,0))
-            total_loss+=loss
-            loss.backward()
-            optimizer.step()
+            if batch_size==1:
+                x=train_exs[i].words 
+                y=[train_exs[i].label==0,train_exs[i].label==1]
+                y_onehot=torch.from_numpy(np.asarray(y,dtype=np.int64)).to(device).float()
+                dan.zero_grad()
+                log_probs=dan.forward(x,batch_size)
+                loss=torch.neg(log_probs).dot(y_onehot)
+                total_loss+=loss
+                loss.backward()
+                optimizer.step()
+            else:
+                x=[train_exs[j].words for j in range(i,i+batch_size)]
+                y=[[train_exs[j].label==0,train_exs[j].label==1] for j in range(i,i+batch_size)]
+                y_onehot=torch.from_numpy(np.asarray(y,dtype=np.int64)).to(device).float()
+                dan.zero_grad()
+                log_probs=dan.forward(x,batch_size)
+                loss_matrix=torch.matmul(torch.neg(log_probs).t(),y_onehot)
+                #equal to the sum of the main diagnol
+                loss=torch.sum(torch.diagonal(loss_matrix,0))
+                total_loss+=loss
+                loss.backward()
+                optimizer.step()
         print(total_loss) 
     return NeuralSentimentClassifier(dan)
 
